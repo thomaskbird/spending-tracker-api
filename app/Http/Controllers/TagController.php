7815e1dde6cff03b6b23e98f9d8bcc34def3fd09
ThomasBird <thomasbird@ThomasBirds-MacBook-Pro.local>
2019-02-17 21:01:04 -0500

    Added initial routes and actions

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Models\Tag;
+
 class TagController extends Controller {
     public function action_create() {
 
     }
 
     public function single($id) {
-
+        $tag = Tag::find($id);
+        return response(json_encode($tag));
     }
 
     public function view() {
-
+        $tags = Tag::paginate(10);
+        return response(json_encode($tags));
     }
 }
--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Models\Transaction;
+use Illuminate\Http\Request;
+
 class TransactionController extends Controller {
-    public function action_create() {
+    public function action_create(Request $request) {
 
     }
 
     public function single($id) {
-
+        $transaction = Transaction::find($id);
+        return response(json_encode($transaction));
     }
 
     public function view() {
-
+        $transactions = Transaction::paginate(10);
+        return response(json_encode($transactions));
     }
 }
