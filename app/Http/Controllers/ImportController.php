f408c67a285199b0bd4189063c430d6ffff90d6e
Tom Bird <thomas.bird1984@gmail.com>
2019-11-07 14:55:25 -0500

    Working on adding imports

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -18,6 +18,12 @@ class ImportController extends Controller {
 
         $user_id = $this->getUserIdFromToken($request->bearerToken());
 
+        // todo: check if first line is header
+        // todo: iterate through file and create transactions
+        // todo: count the number of total transactions imported
+        // todo: record all of the transactions ids created.
+        // todo: modify transactions table to track what is in queue and what has been fully imported.
+
         $import = Import::create([
             'user_id' => $user_id,
             'type' => $input['sourceType'],
@@ -26,11 +32,22 @@ class ImportController extends Controller {
             'record_ids' => ''
         ]);
 
-        return response(json_encode([
-            'status' => true,
-            'data' => [
-                'import' => $import
-            ]
-        ]));
+        $this->extract_and_insert_rows($import);
+
+//        return response(json_encode([
+//            'status' => true,
+//            'data' => [
+//                'import' => $import
+//            ]
+//        ]));
+    }
+
+    private function extract_and_insert_rows($import) {
+        echo storage_path('imports') .' - '. $import->filename; exit;
+        $file = fopen(storage_path('imports'))
+    }
+
+    private function extract_rows() {
+
     }
 }
