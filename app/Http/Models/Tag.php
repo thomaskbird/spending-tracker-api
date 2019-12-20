90cb93b3287e5601565eabf167c9ae8a389d54dd
Tom Bird <thomas.bird1984@gmail.com>
2019-12-19 21:14:56 -0500

    Trouble shooting tag issues

--- app/Http/Models/Tag.php
+++ app/Http/Models/Tag.php
@@ -18,6 +18,6 @@ class Tag extends Model {
     }
 
     public function transactions() {
-        return $this->morphToMany('App\Http\Models\Transaction', 'taggable', 'taggables', 'taggable_id');
+        return $this->morphToMany('App\Http\Models\Transaction', 'taggable', 'taggables', 'taggable_id', 'tag_id');
     }
 }
