04787dea54913d0831d671b1bd0cbbd89a9fafb3
Tom Bird <thomas.bird1984@gmail.com>
2019-12-19 20:45:30 -0500

    Trouble shooting tag issues

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -114,14 +114,14 @@ class TagController extends Controller {
         $end = $end .' 23:59:59';
 
         // todo: this query never returns any transactions
-        $tags = Tag::with(['transactions' => function($query) use ($user_id, $start, $end) {
+        $tags = Tag::where('user_id', $user_id)->with(['transactions' => function($query) use ($user_id, $start, $end) {
             $query
                 ->whereRaw(
                     'user_id = ? AND occurred_at >= ? AND occurred_at <= ?',
                     [$user_id, $start, $end]
                 )
                 ->groupBy(DB::raw('YEAR(occurred_at) DESC, MONTH(occurred_at) DESC'));
-        }])->where('user_id', $user_id)->get();
+        }])->get();
 
         return response(json_encode([
             'status'=> true,
