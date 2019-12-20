dba3209a137cf7cf72c25c548e4c5aec15c9f9f7
Tom Bird <thomas.bird1984@gmail.com>
2019-12-19 21:03:46 -0500

    Trouble shooting tag issues

--- app/Http/Models/Tag.php
+++ app/Http/Models/Tag.php
@@ -22,7 +22,8 @@ class Tag extends Model {
             'App\Http\Models\Transaction',
             'taggable',
             'taggables',
-            'tag_id'
+            'tag_id',
+            'taggable_id'
         );
     }
 }
