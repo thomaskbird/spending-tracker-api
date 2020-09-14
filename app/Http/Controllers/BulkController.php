661a1c849db7a536f1d9eba67950cc3e9d2a9426
Tom Bird <thomas.bird1984@gmail.com>
2020-09-14 18:54:46 -0400

    Finished code for mass transaction deletion

--- app/Http/Controllers/BulkController.php
+++ app/Http/Controllers/BulkController.php
@@ -8,7 +8,7 @@ class BulkController extends Controller {
 
     public function bulk_transaction_remove(Request $request) {
         $transaction_ids = $request->input('transactionIds');
-        $deleted = Transaction::whereIn('id', $transaction_ids)->get()->delete();
+        $deleted = Transaction::whereIn('id', $transaction_ids)->delete();
 
         if($deleted) {
             return response(json_encode([
