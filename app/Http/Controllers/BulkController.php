47d36f4ce187dd10bf60dcc883c2aef8befcdade
Tom Bird <thomas.bird1984@gmail.com>
2020-09-14 18:49:13 -0400

    Return ids

--- app/Http/Controllers/BulkController.php
+++ app/Http/Controllers/BulkController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class BulkController extends Controller {
 
     public function bulk_transaction_remove(Request $request) {
-        $transaction_ids = $request->input['transactionIds'];
+        $transaction_ids = $request->input('transactionIds');
 
         return $transaction_ids;
     }
